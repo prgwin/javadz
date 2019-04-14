dd3851232163e9804d4fbf56673238fad000375c
prgwin <wineml@yandex.ru>
2019-04-14 12:08:46 +0400

    вынесли расчеты в новую переменную

--- src/ru/petrov/task1/Gasoline.java
+++ src/ru/petrov/task1/Gasoline.java
@@ -4,7 +4,7 @@ public class Gasoline {
     public static void main(String[] args) {
         int price = 42;
         int litr = 30;
-        int sum = price * litr;
+        int sum = price*litr;
 
         System.out.println("Цена литра бензина " + price + " руб.");
         System.out.println("Количество литров " + litr);
