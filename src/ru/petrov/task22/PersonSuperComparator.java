d534ec3e7e52427b6366b27eebd0a409823d9922
prgwin <wineml@yandex.ru>
2019-06-19 22:00:41 +0400

    fix tack22 fin

--- src/ru/petrov/task22/PersonSuperComparator.java
+++ src/ru/petrov/task22/PersonSuperComparator.java
@@ -5,8 +5,8 @@ import java.util.Comparator;
 public class PersonSuperComparator implements Comparator<Person> {
     @Override
     public int compare(Person o1, Person o2) {
-        int result = o1.getName().compareTo(o2.getName());
-        if (result != 0) {
+        int result = o1.getName().compareTo(o2.getName()); //Сравниваем по name
+        if (result != 0) {    //Если name равны, сравниваем по age
             return result;
         } else {
             return o1.getAge() - o2.getAge();
